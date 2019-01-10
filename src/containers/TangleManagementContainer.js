fc246171657bacb37980c6c3351c613db645e01b
proof.K <koftwro@gmail.com>
2019-01-10 15:51:13 +0800

    Delete "graph" code and test node input UI

--- src/containers/TangleManagementContainer.js
+++ src/containers/TangleManagementContainer.js
@@ -45,52 +45,25 @@ class TangleManagementContainer extends React.Component{
 	    'host':this.state.NodeHost ,
 	    'port':this.state.NodePort 
 	}));
-
-
-		console.log(this.state.NodeHost);
-
-		console.log(this.state.NodePort);
-
-		console.log(Node.getNodeInfo());
 	
-		this.setState({requestServer : Node});
+		this.setState({requestServer:Node});
 
-		//console.log(this.state.requestServer.get_node_info());
 	}
 
 	InputTangleRoot(e){
 	
 		this.setState({TangleRoot : e.target.value});
-		console.log(this.state.TangleRoot);
 	};
 
 	GraphTangle(e){
+
 		e.preventDefault();	
-		const nodeRadius = 25;
 
+		let nodes = [];
+		
+		nodes.push({});
 
-	  	let c_TangleTips = [];
-	  	let c_nodes = [];
-		  
-
-	  	c_nodes.push({name:'0',transactionHash:this.state.rootTransactionHash,nodeIndex:0,x:0,y:150,});
-
-	  	c_TangleTips.push({name:'0',transactionHash:this.state.rootTransactionHash,nodeIndex:0,x:0,y:150,});
-
-
-
-		let ID = setInterval(()=>{this.GetNextApprovees();},3000); 
-
-		this.setState({
-			nodes: c_nodes,
-			links: [],
-			TangleTips: c_TangleTips,
-			time: 0,
-      			nodeRadius,
-			intervalID: ID,
-			live: 1,
-    			}, );	
-	};
+			};
 
 	Carry(e){
 	
@@ -117,96 +90,17 @@ class TangleManagementContainer extends React.Component{
 
 	GetNextApprovees(){
 	
-	const nodeRadius = 25;
-
-
-		let tangle = "";
-		let c_subTangleTips = [];
-	  	let c_nodes = this.state.nodes;
-	  	let c_links = this.state.links;
-	  	let c_time = this.state.time + 1;
-		let s_node = this.state.requestServer;
-
-		if(this.state.live == 0){
-				return ;
-			}
-		
-
-		if(this.state.subTangleTips.length > 0){
-
-		
-		for(let findTransaction of this.state.subTangleTips){
-
-
-		 const approve_list = s_node.findTransactions({'approvees': [findTransaction.transactionHash]});
-
-	Promise.all([approve_list]).then(([approve_list])=>{
 	
-
-		console.log(approve_list);
-
-	  	if(approve_list.length > 0){	
-
-
-
-		for(let a_transaction of approve_list){
 		
-			let exist = 0;
 			
-			for(let site of c_nodes){
-				if(a_transaction == site.transactionHash){
-					exist = 1;
-				
-				}
-			}
+	};
 
 
-			if(exist == 1){
-
-
-			}
-			else{
-			let c_nodeIndex = c_nodes.length;
-			let c_node_x = findTransaction.x + 500 + 250*Math.random();
-			let c_node_y = findTransaction.y + (200 + approve_list.length*50)*(Math.random() - 0.5);
-
-			c_nodes.push({name:c_nodeIndex.toString(),transactionHash:a_transaction,nodeIndex:c_nodeIndex,x:c_node_x,y:c_node_y,});
-			c_subTangleTips.push({name:c_nodeIndex.toString(),transactionHash:a_transaction,nodeIndex:c_nodeIndex,x:c_node_x,y:c_node_y,});
-			c_links.push({source:c_nodes[c_nodeIndex],target:c_nodes[findTransaction.nodeIndex]});
-
-
-			
-			}
-			
+	Test(e){
 	
-			}
+	console.log(this.state.TangleRoot);
 
-		}
-
-	
-	});
-
-		}
-
-
-			
-
-			}
-
-
-
-
-
-		tangle = {nodes:c_nodes,links:c_links};
-
-
-
-    			this.setState({
-      			nodes: tangle.nodes,
-      			links: tangle.links,
-			subTangleTips: c_subTangleTips,
-      			nodeRadius,
-    			});
+	console.log(this.state.requestServer.getNodeInfo());
 	
 	};
 
@@ -233,6 +127,8 @@ class TangleManagementContainer extends React.Component{
 		Clear={this.Clear.bind(this)}
 
 		/>
+		
+		<button onClick={this.Test.bind(this)} >Test TangleManagementContainer</button>		
 
 		</div>);
 	
